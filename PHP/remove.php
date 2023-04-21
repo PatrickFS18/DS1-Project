868a25a78f909ec3db1d4e17eb462a156aeb7a09
PatrickFS18 <Patrick.souza@aluno.riogrande.ifrs.edu.br>
2023-04-21 17:30:49 -0300

    Criação tabela Historico e data Exclusao

--- PHP/remove.php
+++ PHP/remove.php
@@ -5,11 +5,37 @@ $UserID = $_SESSION["ID"];
 
 if (!empty($_POST["Delete"])) {
     $GameID = $_POST["Delete"];
+    date_default_timezone_set("America/Sao_Paulo");
+    $DeletedG = date('d-m-Y H:i:s');
+    $DataToSave = $conexao->query("SELECT * FROM `registergame` WHERE `UsuarioID`='$UserID[ID]' AND `ID` = '$GameID'");
+    $row = $DataToSave->fetch_assoc();
+    $IDG = $row["ID"];
+    $TituloG = $row["Titulo"];
+    $PlataformaG = $row["Plataforma"];
+    $AnoG =  $row["Ano"];
+    $ImageG =  $row["Image"];
+    # Inserir ao histórico o jogo a ser excluído
+    $Insert = mysqli_prepare(
+        $conexao,
+        "INSERT INTO `Historico` (`ID`,`Titulo`,`Plataforma`,`Ano`,`UsuarioID`,`Image`,`DeleteAT`) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    );
+    $Insert->bind_param(
+        "issiiss",
+        $GameID,
+        $TituloG,
+        $PlataformaG,
+        $AnoG,
+        $UserID["ID"],
+        $ImageG,
+        $DeletedG
+    );
+    $Insert->execute();
+   
     $query = "DELETE FROM `registergame` WHERE `ID` = '$GameID'";
     if(mysqli_query($conexao, $query)){
-        header("location:/PHP/index.php");
+         header("location:/PHP/index.php");
     }else{
-        header("location:/PHP/index.php?msg=err_del");
+         header("location:/PHP/index.php?msg=err_del");
     }
 }
  mysqli_close($conexao);
